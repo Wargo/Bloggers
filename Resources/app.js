f4649b14f7b23432f4ea0e5deffea75a04a8ef42
Guille <guillermo@artvisual.net>
2012-10-23 14:14:37 +0200

    .

--- Resources/app.js
+++ Resources/app.js
@@ -1,6 +1,16 @@
-
+	
 (function() {
 	
 	Mods = require('/modules');
+	
+	if (Ti.Platform.osname === 'andorid') {
+		$$ = require(Mods.android);
+	} else {
+		$$ = require(Mods.iphone);
+	}
+	
+	var MyWin = require(Mods.main);
+	
+	new MyWin().open();
 	
 })();
--- Resources/modules.js
+++ Resources/modules.js
@@ -1,6 +1,25 @@
 
 module.exports = {
 	
+	/*
+	 * Windows
+	 */
+	main:'ui/main',
+	
+	/*
+	 * Styles
+	 */
+	iphone:'ui/styles/iphone',
+	android:'ui/styles/android',
+	
+	/*
+	 * Elements
+	 */
+	
+	
+	/*
+	 * Modules
+	 */
 	
 	
 }
