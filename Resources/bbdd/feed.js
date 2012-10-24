084b88795eb6bc7cb51c331e205597d29eb60d7f
Guille <guillermo@artvisual.net>
2012-10-24 09:49:28 +0200

    -

--- Resources/bbdd/feed.js
+++ Resources/bbdd/feed.js
@@ -1,9 +1,25 @@
 
 module.exports = function(f_callback) {
+	
+	if (Ti.App.Properties.getString('feed', null)) {
+		var result = JSON.parse(Ti.App.Properties.getString('feed'));
+		f_callback(result.data);
+		return;
+	}
 
 	var client = Ti.Network.createHTTPClient({
 		onload: function() {
-			f_callback(this.responseXML);
+			var result = JSON.parse(this.responseText);
+			if (result.status == 'ok') {
+				Ti.App.Properties.setString('feed', this.responseText);
+				f_callback(result.data);
+			} else {
+				Ti.UI.createAlertDialog({
+					title:'Error',
+					message:result.message,
+					ok:'Vale'
+				}).show();
+			}
 		},
 		onerror: function(e) {
 			alert(e);
@@ -11,7 +27,7 @@ module.exports = function(f_callback) {
 		timeout: 15000
 	});
 	
-	client.open('GET', 'http://blog.elembarazo.net/category/bloggers/feed');
+	client.open('GET', 'http://www.servidordeprueba.net/webs/bloggers/feed.php');
 	
 	client.send();
 
--- Resources/ui/main.js
+++ Resources/ui/main.js
@@ -8,47 +8,101 @@ module.exports = function() {
 	
 	var win = Ti.UI.createWindow($$.win);
 	
+	var loader = Ti.UI.createActivityIndicator();
+	win.add(loader);
+	loader.show();
+	
 	var getData = require(Mods.bbdd);
 	getData(setData);
 	
-	var MONTH_MAP = { JAN: 1, FEB: 2, MAR: 3, APR: 4, MAY: 5, JUN: 6, JUL: 7, AUG: 8, SEP: 9, OCT: 10, NOV: 11, DEC: 12 };
+	var logo = Ti.UI.createLabel({
+		text:'Bloggers',
+		color:'#333',
+		font:{fontSize:'20dp', fontWeight:'bold'},
+		top:0,
+		height:'50dp'
+	});
 	
-	function setData(xml) {
-		if (xml === null || xml.documentElement === null) { 
-			return;
-		}
-
-		var items = xml.documentElement.getElementsByTagName("item");
-		var data = [];
-
-		for (var i = 0; i < items.length; i++) {
-			var item = items.item(i);
-			var image;
-			try {
-				var image = item.getElementsByTagNameNS('http://mashable.com/', 'thumbnail').item(0).getElementsByTagName('img').item(0).getAttribute('src');
-			} catch (e) {
-				image = '';
-			}
-
-			data.push({
-				title: getRssText(item, 'title'),
-				link: getRssText(item, 'link'),
-				pubDate: parseDate(getRssText(item, 'pubDate')),
-				image: image
+	var separatorHeader = Ti.UI.createView({
+		height:'5dp',
+		backgroundColor:'#999',
+		top:'50dp',
+		right:'5dp',
+		left:'5dp'
+	});
+	
+	win.add(logo);
+	win.add(separatorHeader);
+	
+	logo.addEventListener('longpress', function() {
+		Ti.App.Properties.removeProperty('feed');
+		win.remove(tableView);
+		getData(setData);
+		loader.show();
+	});
+	
+	var tableView = null;
+	
+	function setData(data) {
+		
+		tableView = Ti.UI.createTableView({
+			top:'50dp',
+			separatorColor:'#8CCC',
+			backgroundColor:'#EEE'
+		});
+		
+		for (i in data) {
+			
+			var row = Ti.UI.createTableViewRow({
+				height:'100dp'
 			});
+			
+			var separator = Ti.UI.createView({
+				height:'1dp',
+				backgroundColor:'#8FFF',
+				top:0
+			});
+			
+			row.add(separator);
+			
+			var content = Ti.UI.createView({
+				layout:'vertical',
+				left:'80dp',
+				top:'5dp',
+				bottom:'5dp',
+				right:'5dp'
+			});
+			
+			var title = Ti.UI.createLabel({
+				text:data[i].title,
+				font:{fontSize:'15dp', fontWeight:'bold'},
+				left:0
+			});
+			
+			var text = Ti.UI.createLabel({
+				text:data[i].description,
+				font:{fontSize:'13dp'},
+				left:0
+			});
+			
+			var image = Ti.UI.createImageView({
+				image:data[i].image,
+				left:'10dp',
+				width:'60dp'
+			});
+			
+			content.add(title);
+			content.add(text);
+			row.add(image);
+			row.add(content);
+			
+			tableView.appendRow(row);
+			
 		}
 		
-		alert(data);
-	}
-	
-	var getRssText = function(item, key) {
-		return item.getElementsByTagName(key).item(0).text;
-	}
-	
-	var parseDate = function(dateString) {
-		var dateParts = dateString.split(' ');
-		var timeParts = dateParts[4].split(':');
-		return MONTH_MAP[dateParts[2].toUpperCase()] + '/' + dateParts[1] + ' ' + timeParts[0] + ':' + timeParts[1];
+		win.add(tableView);
+		loader.hide();
+		
 	}
 	
 	return win;
