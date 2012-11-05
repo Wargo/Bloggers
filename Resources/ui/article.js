e9b0608aed034244d4ae098d60f7b2c62c68cb00
Guillermo <guillermo@artvisual.net>
2012-11-05 13:28:08 +0100

    .

--- Resources/ui/article.js
+++ Resources/ui/article.js
@@ -80,8 +80,9 @@ module.exports = function(article) {
 			} else if (e.index === 1) {
 				var emailDialog = Titanium.UI.createEmailDialog();
 				emailDialog.subject = 'Mira este artículo';
+				emailDialog.html = true;
 				emailDialog.toRecipients = null;
-				emailDialog.messageBody = 'Visita: <br /><a href="http://www.artvisual.net">este artículo</a>';
+				emailDialog.messageBody = 'Visita <a href="http://www.artvisual.net">este artículo</a>';
 				//var f = Ti.Filesystem.getFile('cricket.wav');
 				//emailDialog.addAttachment(f);
 				emailDialog.open();
