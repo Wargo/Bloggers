bd88f95aeaac990c3bafec46969e9b4689e6aecd
Guille <guillermo@artvisual.net>
2012-12-05 17:53:11 +0100

    cache

--- Resources/app.js
+++ Resources/app.js
@@ -16,4 +16,30 @@
 	
 	new MyWin().open();
 	
+	/*
+	 * Clear Cache
+	 */
+	var version = 2;
+	
+	var haveToClear = Ti.App.Properties.getInt('clearCache', version);
+	
+	if (haveToClear <= version) {
+		var cacheFolder = Ti.Filesystem.getFile(Ti.Filesystem.applicationCacheDirectory);
+		var files = cacheFolder.getDirectoryListing();
+		
+		for (i in files) {
+			
+			if (files[i].substring(files[i].length - 4, files[i].length) == '.jpg') {
+				var fileToDelete = Ti.Filesystem.getFile(Ti.Filesystem.applicationCacheDirectory + files[i]);
+				fileToDelete.deleteFile();
+			}
+			
+		}
+		
+		Ti.API.error('Caché limpiada');
+		
+		Ti.App.Properties.setInt('clearCache', version + 1);
+
+	}
+	
 })();
